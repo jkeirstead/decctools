51938192c261855f347fe6c2d07f34514f34c08e
James Keirstead <james.keirstead@gmail.com>
2013-06-24 00:25:57 +0100

    Added license conditions on Elexon data and a function to check for most recent valid date.

--- R/carbon-intensity.r
+++ R/carbon-intensity.r
@@ -2,8 +2,8 @@
 #'
 #' Gets the mix of fuel sources used to generate UK grid electricity between specified dates.  Valid dates are yyyy-mm-dd and must be between 2009-01-01 and the current day; the start must come before the end date.
 #'
-#' @source The underlying data come from BM Reports, \url{http://www.bmreports.com/bsp/bsp_home.htm}, and are used under license from Elexon. However the BM Reports website is hard to use and so this function uses archived data compiled by the Renewable Energy Foundation at \url{http://www.ref.org.uk/fuel/}.
-#'
+#' @source The underlying data come from BM Reports, \url{http://www.bmreports.com/bsp/bsp_home.htm}, and are used under license from Elexon (see disclaimer below). However the BM Reports website is hard to use and so this function uses archived Elexon data compiled by the charity Renewable Energy Foundation and available at no cost at \url{http://www.ref.org.uk/fuel/}.
+#' ELEXON does not and shall not be deemed to warrant or guarantee or make any representation (whether expressly or impliedly) as to the title, completeness, accuracy, reliability, freeness from error, method of preparation, merchantability or fitness for the purpose or for any particular purpose (whether or not any such purpose has been notified to ELEXON) of the Licensed Data and/or any use of or dealing with it.  ELEXON shall have no liability or responsibility whatsoever or howsoever arising (whether directly or indirectly) as a result of or in connection therewith, including in respect of each and all loss of profits, loss of revenue, loss of goodwill, loss of contracts, loss of business or production, indirect loss, economic or consequential loss, claims, demands, proceedings, actions, losses, liabilities, damages, costs, charges and expenses (whether or not any of the foregoing are foreseeable)
 #'
 #' @param start the start date for retrieving data.
 #' @param end the end date for retrieving data
@@ -22,12 +22,15 @@ get_grid_mix <- function(start, end) {
   end <- as.Date(end)
     
   ## Validate inputs
-  if (start < as.Date("2009-01-01") | start > as.Date(Sys.time())) {
-    stop("Invalid start date.  Must be between 2009-01-01 and today")
+  first_date <- as.Date("2009-01-01")
+  last_date <- get_last_date()
+  template <- "Invalid %s date.  Must be between %s and %s."
+  if (start < as.Date("2009-01-01") | start > last_date) {
+    stop(sprintf(template, "start", first_date, last_date))
   }
 
-  if (end < as.Date("2009-01-01") | start > as.Date(Sys.time())) {
-    stop("Invalid end date.  Must be between 2009-01-01 and today")
+  if (end < as.Date("2009-01-01") | start > last_date) {
+    stop(sprintf(template, "end", first_date, last_date))
   }
 
   if (end < start) stop("End date before start date.  Try again.")
@@ -86,4 +89,19 @@ get_grid_carbon <- function(start, end) {
   return(result)
 }
   
+
+#' Gets the most recent date for available data
+#'
+#' Gets the date of the last update to the REF fuels mix database.
+#'
+#' @return a Date object
+get_last_date <- function() {
+  
+  base_url <- "http://www.ref.org.uk/fuel/tablebysp.php"
+  columns <- c("character","numeric", rep("FormattedNumber", 14)) 
+  table <- readHTMLTable(base_url, colClasses=columns)[[2]]
+  last_date <- as.Date(as.character(tail(table$SD, 1)))
+  return(last_date)
+}
+                       
   
