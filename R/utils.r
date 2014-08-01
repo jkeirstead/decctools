37fd97a873d8e0ab5933847bbb462a543401c6dd
James Keirstead <james.keirstead@gmail.com>
2014-08-01 18:11:19 +0100

    Tidied get_lookup_table.

--- R/utils.r
+++ R/utils.r
@@ -79,95 +79,43 @@ get_package_name <- function() {
 #' Gets a lookup table to match IDs across geographies
 #'
 #' Provides a lookup table giving the local authority name and
-#' corresponding IDs for LAU, LAD, MSOA, and LSOA geographies.  Also
-#' returns a flag indicating whether or not a given LAU is urban.  See
-#' \code{\link{LAD_metadata}} for more information.
+#' corresponding IDs for LAD, MSOA, and LSOA geographies.
 #'
-#' @param urban_classes classifications to be considered as urban.
-#' Possible values include 'OU' (other urban), 'LU' (large urban),
-#' 'MU' (major urban), 'SR' (significant rural), 'R80' (80\% rural),
-#' 'R50' (50\% rural)
-#' @param dir (optional) directory to save the lookup table
-#' information
+#' @note As SOA energy data are not available for Northern Ireland,
+#' this method only returns a lookup for England, Wales, and Scotland.
+#'
 #' @return a data frame
 #' @export
-#' @import stringr
-get_lookup_table <- function(urban_classes=c("LU", "MU"), dir) {
+get_lookup_table <- function() {
 
-  ## The lookup data is available in the LSOA electricity demands data
-  ## from DECC so we'll use this rather than rooting around for a
-  ## master copy from ONS
-  url <- "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/49432/4813-llsoa-domestic-elec-est-2010.xls"
+    ## For England and Wales, this is quite straight forward as ONS
+    ## provides a single lookup table.
+    url <- "https://geoportal.statistics.gov.uk/Docs/Lookups/Output_areas_(2011)_to_lower_layer_super_output_areas_(2011)_to_middle_layer_super_output_areas_(2011)_to_local_authority_districts_(2011)_E+W_lookup.zip"
+    file_name <- get_remote_file(url, NA)
+    unzip(file_name, exdir=tempdir())
+    df <- read.csv(file.path(tempdir(), "OA11_LSOA11_MSOA11_LAD11_EW_LUv2.csv"),
+                   stringsAsFactors=FALSE)
+    df <- df[,c("LSOA11CD", "MSOA11CD", "LAD11CD")]
+    names(df) <- c("LSOA", "MSOA", "LAD")
+    
+    ## Scotland however does things its own way. The first file
+    ## provides the lookup between IGZ/MSOA geographies and local
+    ## authorities.  But their Scottish Executive Local Authority
+    ## codes don't match up to the ONS codes.  I've manually built a
+    ## lookup table from
+    ## \url{https://www.scotxed.net/Survey%20documentation/List%20of%20local%20authority%20codes.xls}
+    url <- "http://www.scotland.gov.uk/Resource/Doc/933/0109431.txt"
+    file_name <- get_remote_file(url, dir)
+    scot.df <- read.csv(file_name)
+    data(scotland_igz, envir=environment())
+    scot.df <- merge(scot.df, scotland_igz, by.x="LA_CODE", by.y="scotex")
+    scot.df <- scot.df[,c("IZ_CODE", "lad")]
+    names(scot.df) <- c("MSOA", "LAD")
+    scot.df <- cbind(LSOA=NA, scot.df)
 
-  file_name <- get_remote_file(url, dir)
+    ## Fortunately Northern Ireland doesn't even provide lower level
+    ## energy data so we don't need to worry about this.
 
-  ## Load it into memory.  Only need first 4 columns
-  wb <- tryCatch({
-      loadWorkbook(file_name)
-  }, error=function(e) {
-      message(sprintf("Error loading workbook:\n\n%s\nTried download file from %s.  Email package maintainer to see if URL has changed.  Returning an empty data frame.", e, url))
-      return(NULL)
-  })
-
-  ## If a valid workbook isn't found, return an empty data frame
-  if (is.null(wb)) return(data.frame())
-  
-  data <- readWorksheet(wb, "LLSOA Electricity Domestic", startCol=1, endCol=4)
-  names(data) <- c("LAU1_name", "LAU1_code", "MSOA_code", "LSOA_code")
-
-  ## Get rid of empty lines
-  data <- data[!is.na(data$LAU1_name), ]
-
-  ## Also need to add in the Scottish IGZ codes
-  ## Download the file
-  url <- "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/49424/4828-mlsoa-dom-elec-2010-alldata.xls"
-  file_name <- get_remote_file(url, dir)
-
-  ## Now open up the file and read the data
-  ## Note that it is in two parts: English MLSOAs and Scottish IGZs
-  wb <- loadWorkbook(file_name)
-  scotland_data <- readWorksheet(wb, "MLSOA Domestic Electricity")
-  rm(wb)
-  scotland_data <- scotland_data[,1:3]
-  names(scotland_data) <- c("LAU1_name", "LAU1_code", "MSOA_code")
-  scotland_data <- scotland_data[!is.na(scotland_data$LAU1_name) &
-                                 str_sub(scotland_data$MSOA_code, end=1)=="S", ]
-  scotland_data <- cbind(scotland_data, LSOA_code=NA)
-
-  ## Combine the two data sets
-  data <- rbind(data, scotland_data)
-  
-  ## Some manual checking found a couple discrepancies in the names
-  ## Tweak these to match unique(subset(tmp, is.na(LAU1_code) and
-  ## !(country%in%c("Northern Ireland")))$LAU1_name)
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Argyll \\& Bute UA Island", "Argyll and Bute"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Edinburgh, City of", "City of Edinburgh"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Dumfries \\& Galloway", "Dumfries and Galloway"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Eilean Siar \\(Western Isles\\)", "Eilean Siar"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Perth \\& Kinross", "Perth and Kinross"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Cheshire West \\& Chester", "Cheshire West and Chester"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Bristol, City of", "City of Bristol"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Kingston upon Hull, City of", "City of Kingston upon Hull"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Rhondda, Cynon, Taff", "Rhondda Cynon Taf"))
-  data <- mutate(data, LAU1_name=str_replace(data$LAU1_name,
-                           "Vale of Glamorgan, The", "The Vale of Glamorgan"))
-
-  ## Now load in the urban classification.  All the detailed
-  ## processing for this is in the benchmarking paper.
-  data(LAD_metadata, envir=environment())
-  meta <- mutate(LAD_metadata, urban=(LAD_metadata$urban_class %in% urban_classes))
-  meta <- meta[,c("name", "short_code", "long_code", "urban", "country")]
-
-   ## Need to keep Scotland and Northern Ireland too
-  tmp <- merge(data, meta, by.x="LAU1_name", by.y="name", all.y=TRUE) 
-  return(tmp)
+    tmp <- rbind(df, scot.df)
+    return(tmp)
 }
